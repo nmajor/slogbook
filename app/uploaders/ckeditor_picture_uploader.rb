3281a179f06cf71685b363a1721a37f6086a25e8
Nicholas Major <nic.major.d@gmail.com>
2013-09-18 05:04:52 +0000

    Changed image upload path and removed options in ckeditor

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -3,8 +3,8 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
 
   # Include RMagick or ImageScience support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
@@ -13,7 +13,8 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/ckeditor/pictures/#{model.id}"
+    # "uploads/ckeditor/pictures/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{Time.now.year}/#{Time.now.month}/#{Time.now.day}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
