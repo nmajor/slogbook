d78f37c55d94822650ae483b8d2733be87a7230b
nmajor <nic.major.d@gmail.com>
2013-10-07 19:11:42 +0000

    Changed User Avatar uploader to crop to square and changed sizes of versions

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -25,7 +25,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_limit => [260, 260]
+  process :resize_to_fit => [250, 250]
   #
   # def scale(width, height)
   #   # do something
@@ -33,7 +33,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [50, 50]
+    process :resize_to_limit => [40, 40]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
