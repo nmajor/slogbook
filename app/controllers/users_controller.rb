4700660c3f86d2b228258d9385dd102ea9fe8500
Nicholas Major <nic.major.d@gmail.com>
2013-11-20 22:41:32 +0000

    Added sort to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @users = User.all
+    @users = User.order(created_at: :desc)
 
     respond_to do |format|
       format.html # index.html.erb
